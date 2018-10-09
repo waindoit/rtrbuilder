45e2b09d45dae3696e4e3d6688d6818cf08f1cc8
hanny mahfuz <mahakasih@gmail.com>
2018-10-09 19:49:17 +0700

    paging

--- index.php
+++ index.php
@@ -110,7 +110,9 @@
     <!-- Main JS-->
     <script src="js/main.js"></script>
      <!-- <script src="https://ajax.googleapis.com/ajax/gantt/libs/jquery/3.1.0/jquery.min.js"></script> -->
-  <script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+  <!-- <script type="text/javascript" src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script> -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
   <script type="text/javascript">
       $(document).ready( function () {
